85e511eaac23fb70f07ab7affd0e6298a9d37028
Barak Korren <bkorren@redhat.com>
2024-05-06 11:34:55 +0300

    fix: vscode shortcut exits OSH
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- dot_oh-my-bash/custom/plugins/vscodium-shortcut/vscodium-shortcut.plugin.sh
+++ dot_oh-my-bash/custom/plugins/vscodium-shortcut/vscodium-shortcut.plugin.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-# exit if we already have a "code" executable
-type -t code >> /dev/null && exit 0
+_vscodium_shortcut_plugin() {
+    # exit if we already have a "code" executable
+    type -t code >> /dev/null && return 0
 
-# Exit if we don't hav flatpak
-type -t flatpak >> /dev/null || exit 0
+    # Exit if we don't hav flatpak
+    type -t flatpak >> /dev/null || return 0
 
-# Exit is vscodium is not installed
-flatpak info com.vscodium.codium >> /dev/null || exit 0
+    # Exit is vscodium is not installed
+    flatpak info com.vscodium.codium >> /dev/null || return 0
 
-alias code='flatpak run com.vscodium.codium'
+    alias code='flatpak run com.vscodium.codium'
+}
+
+_vscodium_shortcut_plugin
+unset -f _vscodium_shortcut_plugin
